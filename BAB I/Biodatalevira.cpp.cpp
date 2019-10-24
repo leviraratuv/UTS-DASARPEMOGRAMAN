85e396cb4fa741e83aa62e8e046e56bb637eb2d3
leviraratuv <ratulevira@gmail.com>
2019-10-25 03:22:44 +0700

    uts-dasarpemograman

---
+++ BAB I/Biodatalevira.cpp.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << " Program Biodata! " << endl;
+    cout << " --------------------- " << endl;
+    cout << " Nama : Levira Ratu Vitaloka " << endl;
+    cout << " Umur : 18 " << endl;
+    cout << " Alamat : Sumedang " << endl;
+
+    return 0;
+
+}
